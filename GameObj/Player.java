d3f507cd93430b0a3827a44db23f5a0e6e660fa0
lee mih duwk <117137275+rhymLele@users.noreply.github.com>
2023-03-02 10:06:56 +0700

    Update Player.java

--- GameObj/Player.java
+++ GameObj/Player.java
@@ -8,64 +8,82 @@ import javax.swing.ImageIcon;
 
 public class Player {
 	
-	public Player(){
-		this.image = new ImageIcon(getClass().getResource("/game/image/plane.png")).getImage();
+	public Player()
+	{
+		this.image=new ImageIcon(getClass().getResource("/game/image/plane.png")).getImage();
 		this.image_speed=new ImageIcon(getClass().getResource("/game/image/plane_speed.png")).getImage();
 	}
+	
+	
+
+	
+	
 	
 	
 	public static final double PLAYER_SIZE=64;
 	private double x;
 	private double y;
-	private final float MAX_SPEED=1f;
-	private float speed=0f;
 	private float angle=0f;
+	private final float MAX_SPEED=1f;
+	private float speed =0f;
 	private final Image image;
 	private final Image image_speed;
 	private boolean speedUp;
-	
-	public void changeLocation(double x, double y) {
-		this.x = x;
-		this.y =y;
+	public void changeLocation(double x,double y)
+	{
+		this.x=x;
+		this.y=y;
 	}
-	
-	public void changeAngle(float angle) {
-		if(angle<0) {
-			angle = 359;
-		}else if(angle >359) {
-			angle = 0;
-		}						//chinh toa do sao cho ko nho hon 0 va lon hon 360
-		this.angle=angle;
-		
+	public void update()
+	{
+		x+=Math.cos(Math.toRadians(angle))*speed;
+		y+=Math.sin(Math.toRadians(angle))*speed;
 	}
-	
-	public void draw(Graphics2D g2) {
-		AffineTransform oldFrameform = g2.getTransform();
-		g2.translate(x, y);
-		AffineTransform tran = new AffineTransform();
-		tran.rotate(Math.toRadians(angle+90), PLAYER_SIZE/2, PLAYER_SIZE/2);
-		g2.drawImage(speedUp?image_speed:image, tran, null);
-		
-		
-		g2.setTransform(oldFrameform);
+	public void changeAngle(float angle)
+	{
+		if(angle<0)
+		{
+			angle=359;
+		}else if(angle>359)
+		{
+			angle=0;
+		}this.angle=angle;
 	}
-	
+	public void draw(Graphics2D g2)
+	{
+		AffineTransform oldTranform=g2.getTransform();
+		
+		g2.translate(x,y);
+		AffineTransform tran=new AffineTransform();
+		tran.rotate(Math.toRadians(angle+45),PLAYER_SIZE/2,PLAYER_SIZE/2);
+		g2.drawImage(image, tran,null); 
+		g2.setTransform(oldTranform);
+	}
 	public double getX() {
-		return x;	
+		return x;
 	}
-	
 	public double getY() {
 		return y;
 	}
-	
-	public float getAngle(){
+	public float getAngle() {
 		return angle;
 	}
-	public void speedUp() {
+	public void speedUp()
+	{
 		speedUp=true;
+		if(speed>MAX_SPEED)
+		{
+			speed=MAX_SPEED;
+		}else
+		{
+			speed+=0.01f;
+		}
 	}
-	public void speedDown() {
+	public void speedDown()
+	{
 		speedUp=false;
+		if(speed<0) speed=0;
+		else speed-=0.003f;
 	}
 }
 	
