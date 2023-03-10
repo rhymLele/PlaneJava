5e8a1c1d6bb54c39dc0fac5da6cff1b1bfb24055
lee mih duwk <117137275+rhymLele@users.noreply.github.com>
2023-03-10 07:27:13 +0700

    Add boom and effect boom object

---
+++ src/game/obj/ModelBoom.java
@@ -0,0 +1,27 @@
+package game.obj;
+
+public class ModelBoom {
+	double size;
+	float angle;
+	public double getSize() {
+		return size;
+	}
+	public void setSize(double size) {
+		this.size = size;
+	}
+	public float getAngle() {
+		return angle;
+	}
+	public void setAngle(float angle) {
+		this.angle = angle;
+	}
+	public ModelBoom(double size,float angle)
+	{
+		this.size=size;
+		this.angle=angle;
+	}
+	public ModelBoom()
+	{
+		
+	}
+}
